ffaa61d474e5280e9448ca53094b5ff4443192fb
Justine Frank <jf.laelath@gmail.com>
2026-05-28 17:08:52 -0400

    can compute concurrently

--- theories/Monads/STMDefs.v
+++ theories/Monads/STMDefs.v
@@ -12,7 +12,9 @@ From Corelib Require Import PrimString PrimInt63.
 From Crane Require Extraction.
 From Crane Require Import Monads.ITree Monads.IODefs External.VectorDefs Utils.HMap Utils.HAList.
 
-From ITree Require Import Basics.Basics.
+From ITree Require Import Basics.Basics Basics.CategoryOps.
+
+
 
 Import Basics.Monads.
 
@@ -130,7 +132,7 @@ Definition handle_tvar_log {K} {V : K -> Type} {M} `{HMap (nat_key K) (nat_key_t
   (m : M) : forall {A}, tvarE V A -> itree E (A * M) :=
   fun _ t =>
     match t with
-    | NewTVar _ _ => ITree.spin
+    | NewTVar _ _ => ITree.spin (* error for now, need to figure out how to deal with these *)
     | ReadTVar tv =>
         (let '(mk_tvar _ n k) in TVar _ T := tv return TVar V T -> itree E (T * M) in
           match lookup (n, k) m with
@@ -154,7 +156,7 @@ CoFixpoint atomically {K} {V : K -> Type} `{EqDec K eq} {E} `{tvarE V -< E} `{at
         | TauF t => Tau (_atomic m t)
         | VisF (inr1 e) k => '(x, m) <- handle_tvar_log m e ;; Tau (_atomic m (k x))
         | VisF (inl1 Retry) _ => Ret None
-        end) empty t0)))
+        end) HMap.empty t0)))
     (fun oa =>
       match oa with
       | Some a => Ret a
@@ -170,18 +172,20 @@ Definition orElse {A} {K} {V : K -> Type} `{EqDec K eq} (t1 t2 : itree (stmE V) 
     | TauF t => Tau (_orElse m t)
     | VisF (inr1 e) k => '(x, m) <- handle_tvar_log m e ;; Tau (_orElse m (k x))
     | VisF (inl1 Retry) _ => t2
-    end) empty t1.
+    end) HMap.empty t1.
 
 Definition handle_tvars {E} {K} `{EqDec K eq} (V : K -> Type) :
   tvarE V ~> stateT (halist (nat_key K) (nat_key_type V)) (itree E) :=
   fun _ t m =>
     match t with
-    | NewTVar _ _ => ITree.spin
+    | NewTVar k v =>
+        let n := S (fold (fun '(existT _ (n, _) _) acc => max n acc) 0 m) in
+        Ret (add (n, k) v m, mk_tvar V n k)
     | ReadTVar x =>
         let '(mk_tvar _ n k) := x in
         match lookup (n, k) m with
         | Some v => Ret (m, v)
-        | None => ITree.spin
+        | None => ITree.spin (* error *)
         end
     | WriteTVar x v =>
         (let '(mk_tvar _ n k) := x in
@@ -190,6 +194,10 @@ Definition handle_tvars {E} {K} `{EqDec K eq} (V : K -> Type) :
 
 Variant forkE : Type -> Type :=
 | Fork : forkE bool.
+
+Definition fork {E} `{forkE -< E} (t1 t2 : itree E unit) : itree E unit :=
+  b <- trigger Fork ;;
+  if (b : bool) then t1 else t2.
 
 Variant scheduleE : Type -> Type :=
 | Schedule (n : nat) : scheduleE {m : nat | m < n}.
@@ -204,6 +212,7 @@ Fixpoint signth {A} (l : list A) (n : {m : nat | m < length l}) : A :=
     end H
   end.
 
+(* arranged so that a scheduleE handler that always returns 0 will be a round-robin scheduler *)
 CoFixpoint schedule {E F} `{F -< E} (l : list (itree (atomicE F +' forkE +' E) unit)) : itree (scheduleE +' E) unit :=
   match l with
   | [] => Ret tt
@@ -215,29 +224,35 @@ CoFixpoint schedule {E F} `{F -< E} (l : list (itree (atomicE F +' forkE +' E) u
           let t := signth l n in
           match observe t with
           | RetF _ => schedule (l1 ++ l2)
-          | TauF t => schedule (l1 ++ [t] ++ l2)
+          | TauF t => schedule (l1 ++ l2 ++ [t])
           | VisF (inl1 e) k =>
               match e, k with
-              | Atomic _ t, k => a <- translate subevent t ;; schedule (l1 ++ [k a] ++ l2)
+              | Atomic _ t, k => a <- translate subevent t ;; schedule (l1 ++ l2 ++ [k a])
               end
           | VisF (inr1 (inl1 e)) k =>
               match e, k with
-              | Fork, k => schedule (l1 ++ [k true; k false] ++ l2)
+              | Fork, k => schedule (l1 ++ l2 ++ [k true; k false])
               end
-          | VisF (inr1 (inr1 e)) k => Vis (subevent _ e) (fun x => schedule (l1 ++ [k x] ++ l2))
+          | VisF (inr1 (inr1 e)) k => Vis (subevent _ e) (fun x => schedule (l1 ++ l2 ++ [k x]))
           end)
   end.
 
+Definition h_rr {E} : scheduleE ~> itree E :=
+  fun _ e =>
+    match e with
+    | Schedule n =>
+        match n with
+        | 0 => ITree.spin (* error *)
+        | S m => Ret (exist _ 0 (PeanoNat.Nat.lt_0_succ m))
+        end
+    end.
 
+Definition schedule_rr {E F} `{F -< E} (l : list (itree (atomicE F +' forkE +' E) unit)) : itree E unit :=
+  interp (case_ h_rr (id_ _)) (schedule l).
 
 Definition newTVar {E K V} `{tvarE V -< E} (k : K) (a : V k) : itree E (TVar V (V k)) := embed (NewTVar k a).
 Definition readTVar {E K} {V : K -> Type} `{tvarE V -< E} {A} (v : TVar V A) : itree E A := embed (ReadTVar v).
 Definition writeTVar {E K} {V : K -> Type} `{tvarE V -< E} {A} (v : TVar V A) (a : A) : itree E unit := embed (WriteTVar v a).
-
-Crane Extract Inlined Constant atomically => "stm::atomically([&] { return %a0; })".
-Crane Extract Inlined Constant orElse => "stm::orElse<%t0>(%a0, %a1)".
-Crane Extract Inlined Constant retry => "stm::retry<%t0>()".
-Crane Extract Inlined Constant newTVar => "stm::newTVar(%a1)".
 
 Definition check {K} {V : K -> Type} (b : bool) : itree (stmE V) unit :=
   if b then Ret tt else retry.
@@ -246,3 +261,88 @@ Definition modifyTVar {K} {V : K -> Type} {A : Type} (a : TVar V A) (f : A -> A)
   val <- readTVar a ;;
   writeTVar a (f val) ;;
   Ret tt.
+
+Section example.
+
+  #[global] Instance EqDec_unit : EqDec unit eq :=
+    fun x y => match x, y with tt, tt => left eq_refl end.
+
+  Definition nats (_ : unit) : Type := nat.
+
+  Definition fib_trans (x0 x1 : TVar nats nat): itree (stmE nats) unit :=
+    v0 <- readTVar x0 ;;
+    v1 <- readTVar x1 ;;
+    writeTVar x0 v1 ;;
+    writeTVar x1 (v0 + v1).
+
+  Definition fib (n : nat) : itree (atomicE (tvarE nats) +' forkE +' tvarE nats) unit :=
+    x <- newTVar tt 0 ;;
+    y <- newTVar tt 1 ;;
+    (fix fib_ n :=
+      match n with
+      | 0 => Ret tt
+      | S n => fork (atomically (fib_trans x y)) (fib_ n)
+      end) n.
+
+  Definition run_concurrent {K} `{EqDec K eq} (V : K -> Type) (t : itree (atomicE (tvarE V) +' forkE +' tvarE V) unit) : itree void1 (halist _ _ * unit) :=
+    let s : stateT _ _ _ := interp (handle_tvars V) (schedule_rr [t]) in
+    s HMap.empty.
+
+  Definition force {E A} (n : nat) (t : itree E A) : option A :=
+    match observe (burn n t) with
+    | RetF a => Some a
+    | _ => None
+    end.
+
+  Definition unwrap {E A} : option A -> itree E A :=
+    fun o => match o with None => ITree.spin | Some a => Ret a end.
+
+  Compute (force 1000 (run_concurrent nats (fib 8))).
+
+  Definition takeMVar {K} {V : K -> Type} {A} (t : TVar V (option A)) : itree (stmE V) A :=
+    v <- readTVar t ;;
+    match v with
+    | None => retry
+    | Some a => writeTVar t None ;; Ret a
+    end.
+
+  Definition putMVar {K} {V : K -> Type} {A} (t : TVar V (option A)) (a : A) : itree (stmE V) unit :=
+    v <- readTVar t ;;
+    match v with
+    | None => writeTVar t (Some a)
+    | Some _ => retry
+    end.
+  
+  Inductive Types :=
+  | Nat : Types
+  | Opt : Types -> Types.
+
+  Definition decide_types (x y : Types) : {x = y} + {x <> y}.
+    decide equality.
+  Defined.
+
+  #[global] Instance EqDec_Types : EqDec Types eq := decide_types.
+
+  Fixpoint Types_Type (type : Types) : Type :=
+    match type with
+    | Nat => nat
+    | Opt t => option (Types_Type t)
+    end.
+
+  Definition message_passing : itree (atomicE (tvarE Types_Type) +' forkE +' tvarE Types_Type) unit :=
+    mv <- newTVar (Opt Nat) None ;;
+    done <- newTVar Nat 0 ;;
+    fork (v1 <- atomically (takeMVar mv) ;; v2 <- atomically (takeMVar mv) ;; atomically (putMVar mv (v1 + v2)))
+         (atomically (putMVar mv 3) ;; atomically (putMVar mv 4) ;; atomically (r <- takeMVar mv ;; writeTVar done r)).
+
+  Compute (force 100 (run_concurrent Types_Type message_passing)).
+
+
+End example.
+
+
+
+Crane Extract Inlined Constant atomically => "stm::atomically([&] { return %a0; })".
+Crane Extract Inlined Constant orElse => "stm::orElse<%t0>(%a0, %a1)".
+Crane Extract Inlined Constant retry => "stm::retry<%t0>()".
+Crane Extract Inlined Constant newTVar => "stm::newTVar(%a1)".
